c0bf2670c7d1262c57f8edbec0723285836350a3
yesboss102 <yessboss102@yandex.ru>
2021-10-26 04:01:42 +0500

    changes

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "baltikbankcoin")
+set(CN_CURRENCY_DISPLAY_NAME "BaltikBankCoin")
+set(CN_CURRENCY_TICKER "BaltikBankCoin")
